d56e1e258d5766958aff7306107f887bc9e90e53
Your Name <you@example.com>
2022-07-31 14:41:51 +0200

    1

---
+++ main.py
@@ -0,0 +1,65 @@
+# game about поле и развитие
+from random import random
+
+
+def main():
+    pole = pole_c()
+    zaic1= zaic(1,2,10)
+    pole.add_object(zaic1)
+
+    while True:
+        draw_pole(pole)
+        pole.iteration
+
+def print_char(x, y, char):
+        print("\033["+str(y)+";"+str(x)+"H"+char)
+
+def draw_pole(pole):
+    pass
+    
+    
+
+
+class pole_c():
+    def __init__(o):
+        o.objects = list()
+
+    def add_object( o, object  ):
+        o.objects.append(object) 
+
+    def iteration(o):
+        for object in o.objects:
+            object.next()
+
+
+class zaic():
+    def __init__(o,x=1, y=1, id = None):
+        if id is None : id = gen_unic_id()
+        o.id = id
+        o.x = x
+        o.y = y
+
+    def move_up(o):
+        o.x = o.x + 1
+        
+    def next(o):
+        pass
+
+
+def gen_unic_id():
+    return 1                # TODO : make random id
+
+
+class sushestvo:
+    x = 0
+    y = 0
+    id = 0
+    def __init__(t, id):
+        t.id = id
+
+        
+
+
+
+if __name__ == "__main__":
+    main()
